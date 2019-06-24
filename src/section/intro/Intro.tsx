3e8c5dc8e6f5f63354c8b36cfa5fe70808c4e3b7
Alexey Astafev <efsneiron@gmail.com>
2019-06-24 13:59:11 +0300

    added images

--- src/section/intro/Intro.tsx
+++ src/section/intro/Intro.tsx
@@ -10,6 +10,10 @@ import DoneIcon from '@material-ui/icons/Done';
 import DevelopIcon from '@material-ui/icons/DeveloperBoard';
 import AdminstrationIcon from '@material-ui/icons/Computer';
 import SupportIcon from '@material-ui/icons/ContactSupport';
+import development from './images/development.jpeg';
+import guarantee from './images/guarantee.jpeg';
+import administration from './images/administration.jpeg';
+import support from './images/support.jpeg';
 
 const styles = (theme: Theme) => createStyles({
     slider: {
@@ -41,7 +45,7 @@ const styles = (theme: Theme) => createStyles({
         background: 'none'
     },
     action: {
-        color: 'rgba(255, 255, 255, .3)'
+        color: 'rgba(255, 255, 255, .7)'
     },
     selected: {
         color: '#fff !important'
@@ -54,28 +58,28 @@ interface Props extends WithStyles<typeof styles> {
 const slides = [
     {
         id: 0,
-        background: 'https://avatars.mds.yandex.net/get-pdb/49816/2102e48b-64b8-4efa-b8ad-f551299c13ab/s1200',
+        background: development,
         text: 'Developing',
         icon: <DevelopIcon />,
         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     },
     {
         id: 1,
-        background: 'https://avatars.mds.yandex.net/get-pdb/38069/850196c4-39e4-4548-bc9b-43a564458809/s1200',
+        background: administration,
         text: 'Administration',
         icon: <AdminstrationIcon />,
         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
     },
     {
         id: 2,
-        background: 'https://avatars.mds.yandex.net/get-pdb/236760/0df63c66-6cd8-4310-927c-8b40120d8d1f/s1200',
+        background: support,
         text: 'Consultation',
         icon: <SupportIcon />,
         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
     },
     {
         id: 3,
-        background: 'https://avatars.mds.yandex.net/get-pdb/236760/0df63c66-6cd8-4310-927c-8b40120d8d1f/s1200',
+        background: guarantee,
         text: 'Guarantee',
         icon: <DoneIcon />,
         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
@@ -98,7 +102,7 @@ const Intro = ({ classes }: Props) => {
                             className={classes.slide}
                             style={{
                                 opacity: slide.id === index ? 1 : 0,
-                                background: `linear-gradient(rgba(0,0,0,.2), rgba(0,0,0,.2)), url(${item.background}) center / cover no-repeat`
+                                background: `linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.4)), url(${item.background}) center / cover no-repeat`
                                 // background: `url(${item.background}) center / cover no-repeat`
                             }}
                         >
--- typings/images/index.d.ts
+++ typings/images/index.d.ts
@@ -6,3 +6,11 @@ declare module '*.png' {
     const content: string;
     export default content;
 }
+declare module '*.jpg' {
+    const content: string;
+    export default content;
+}
+declare module '*.jpeg' {
+    const content: string;
+    export default content;
+}
